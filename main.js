db186778fdd97e11a0c69ee7e61526f55a140009
Gabriel <gabozerbino@gmail.com>
2022-04-08 16:11:38 -0300

    Genera dinamicamente las preguntas

--- main.js
+++ main.js
@@ -8,7 +8,121 @@ const score = {
   "C" : 0,
   "D" : 0,
 }
+
+const questions = [
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem neque molestias ut magnam deserunt?',
+    answers: [
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'A'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'B'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'C'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'D'
+      }
+    ]
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem neque molestias ut magnam deserunt?',
+    answers: [
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'D'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'C'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'A'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'B'
+      }
+    ]
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem neque molestias ut magnam deserunt?',
+    answers: [
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'A'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'B'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'C'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'D'
+      }
+    ]
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem neque molestias ut magnam deserunt?',
+    answers: [
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'B'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'D'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'C'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'A'
+      }
+    ]
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem neque molestias ut magnam deserunt?',
+    answers: [
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'A'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'C'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'B'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        group: 'D'
+      }
+    ]
+  },
+];
+
 let questionActive = 1;
+const swiperWrapper = document.querySelector('.swiper-wrapper');
+questions.forEach((question, index) => {
+  swiperWrapper.innerHTML += quizzToHTML(question, index + 1);
+});
+
 const swiper = new Swiper('.swiper', {
   speed: 400,
   spaceBetween: 100,
@@ -29,23 +143,22 @@ answers.forEach(answer => {
 
 const btn = document.querySelector('.btn.confirm-slection');
 btn.addEventListener('click', () => {
-  console.log('clicked');
   const selected = document.querySelector(`.quizz[data-onquestion="${questionActive}"] .answer.selected`);
   if (selected) {
     const answer = selected.getAttribute('data-grupo');
     score[answer] ++;
-    questionActive ++;
-    console.log(score);
     if (!swiper.slideNext()) {
       showPopup('Resultado', `
-          <ul>
-            <li>A: ${score.A}</li>
-            <li>B: ${score.B}</li>
-            <li>C: ${score.C}</li>
-            <li>D: ${score.D}</li>
-          </ul>
-            `);
+      <ul>
+        <li>A: ${score.A}</li>
+        <li>B: ${score.B}</li>
+        <li>C: ${score.C}</li>
+        <li>D: ${score.D}</li>
+      </ul>
+      `);
+      return
     }
+    questionActive ++;
   }
   else {
     showPopup('Error', 'Debes seleccionar una respuesta');
@@ -74,3 +187,21 @@ btnPopup.addEventListener('click', () => {
     popup.classList.remove('show');
   }, 150);
 });
+
+function quizzToHTML(quizz, index) {
+  const html = `
+    <div class="swiper-slide">
+      <div class="quizz" data-onquestion="${index}">
+        <div class="question">
+          <span>${quizz.question}</span>
+        </div>
+        <div class="answers">
+          ${quizz.answers.map(answer => `
+            <span class="answer" data-grupo="${answer.group}" data-selected="false">${answer.text}</span>
+          `).join('')}
+        </div>
+      </div>
+    </div>
+  `;
+  return html;
+}
