fa22edd34eaddcc217b1df56f78a3814e86c8932
Gabriel <gabozerbino@gmail.com>
2022-04-08 11:19:11 -0300

    finalizado equis de

--- main.js
+++ main.js
@@ -1,6 +1,78 @@
 import './style.css'
+import Swiper from 'swiper';
+import "swiper/css";
+import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const score = {
+  "A" : 0,
+  "B" : 0,
+  "C" : 0,
+  "D" : 0,
+}
+let questionActive = 1;
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  spaceBetween: 100,
+  allowTouchMove: false,
+});
+
+const answers = document.querySelectorAll('.answer');
+
+answers.forEach(answer => {
+  answer.addEventListener('click', () => {
+    const answered = document.querySelector('.answer.selected');
+    if (answered) {
+      answered.classList.remove('selected');
+    }
+    answer.classList.add('selected');
+  });
+});
+
+const btn = document.querySelector('.btn.confirm-slection');
+btn.addEventListener('click', () => {
+  console.log('clicked');
+  const selected = document.querySelector(`.quizz[data-onquestion="${questionActive}"] .answer.selected`);
+  if (selected) {
+    const answer = selected.getAttribute('data-grupo');
+    score[answer] ++;
+    questionActive ++;
+    console.log(score);
+    if (!swiper.slideNext()) {
+      showPopup('Resultado', `
+          <ul>
+            <li>A: ${score.A}</li>
+            <li>B: ${score.B}</li>
+            <li>C: ${score.C}</li>
+            <li>D: ${score.D}</li>
+          </ul>
+            `);
+    }
+  }
+  else {
+    showPopup('Error', 'Debes seleccionar una respuesta');
+  }
+});
+
+const popup = document.querySelector('.popup');
+const popupContainer = document.querySelector('.popup-main');
+const btnPopup = document.querySelector('.btn.close');
+
+//showPopup function
+function showPopup(title, html) {
+  const popupTitle = document.querySelector('.popup-title');
+  const popupContext = document.querySelector('.popup-context');
+  popupTitle.innerText = title;
+  popupContext.innerHTML = html;
+  popup.classList.add('show');
+  setTimeout(() => {
+    popupContainer.classList.add('show');
+  }, 100);
+}
+
+btnPopup.addEventListener('click', () => {
+  popupContainer.classList.remove('show');
+  setTimeout(() => {
+    popup.classList.remove('show');
+  }, 150);
+});
